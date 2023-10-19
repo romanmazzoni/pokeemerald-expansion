e9af1d3847d55ac464d121d490551df01e497501
DESKTOP-CBTULA3\Roman <magiccoat2000@gmail.com>
2023-10-19 15:27:38 -0400

    fixed it fr this time

--- include/battle.h
+++ include/battle.h
@@ -697,14 +697,14 @@ struct BattleStruct
     gBattleMons[battlerId].type1 = type;            \
     gBattleMons[battlerId].type2 = type;            \
     gBattleMons[battlerId].type3 = type;    \
-    gBattleMons[battlerId].type4 = type;
+    gBattleMons[battlerId].type4 = type; \
 }
 #define RESTORE_BATTLER_TYPE(battlerId)                                                     \
 {                                                                                           \
     gBattleMons[battlerId].type1 = gSpeciesInfo[gBattleMons[battlerId].species].types[0];   \
     gBattleMons[battlerId].type2 = gSpeciesInfo[gBattleMons[battlerId].species].types[1];   \
     gBattleMons[battlerId].type3 = gSpeciesInfo[gBattleMons[battlerId].species].types[2];                                            \
-    gBattleMons[battlerId].type4 = gSpeciesInfo[gBattleMons[battlerId].species].types[3];
+    gBattleMons[battlerId].type4 = gSpeciesInfo[gBattleMons[battlerId].species].types[3];  \
 }
 
 
