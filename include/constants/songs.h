402339bb0567ed3f2f407e73879195b33c45fea0
DESKTOP-CBTULA3\Roman <magiccoat2000@gmail.com>
2023-12-26 02:44:47 -0500

    Through the fire and the flames

--- include/constants/songs.h
+++ include/constants/songs.h
@@ -545,6 +545,7 @@
 #define PH_NURSE_SOLO               609
 
 #define MUS_PONCE                   610
+#define MUS_THROUGHTHEFIREANDFLAMES 611
 
 #define MUS_ROUTE118                0x7FFF  // Map is split into 2 music sections. controlled by GetCurrLocationDefaultMusic().
 
