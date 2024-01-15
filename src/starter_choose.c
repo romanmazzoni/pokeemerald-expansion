9bfdd07279590a7e681adf8b63c4421c53480d7c
DESKTOP-CBTULA3\Roman <magiccoat2000@gmail.com>
2024-01-14 23:24:19 -0500

    starter choose fix

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,9 +112,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_OSHAWOTT,
-    SPECIES_SCORBUNNY,
     SPECIES_TURTWIG,
+    SPECIES_SCORBUNNY,
+    SPECIES_OSHAWOTT,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
